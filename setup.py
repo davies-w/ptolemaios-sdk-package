7732aea235a787b10c25ee0d930ca26b26564028
Winton Davies <wdavies@cs.stanford.edu>
2023-04-20 13:48:20 -0700

    updated setup.py and created my_cool_test_method

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+        name='ptolemaios',
+        version='0.0.1',
+        description='My private package from private github repo',
+        url='git@github.com:davies-w/ptolemaios-sdk-package.git',
+        author='Winton Davies',
+        author_email='wdavies@cs.stanford.edu',
+        license='unlicense',
+        packages=['ptolemaios'],
+        zip_safe=False
+    )

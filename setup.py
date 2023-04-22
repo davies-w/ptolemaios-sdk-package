06043540f6cec3e4bb76e07d2831d4c42a4e551f
Winton Davies <wdavies@cs.stanford.edu>
2023-04-21 20:17:55 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-# Copyright 2022 Harsimran Singh Maan
+# Copyright 2023 Winton Davies
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
